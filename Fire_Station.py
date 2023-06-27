4013b392e623fded38b537f869977f2ee6a9ff37
Kim TW <twoo19999@gmail.com>
2023-06-28 00:21:09 +0900

    2023-06-28 소방서 GUI 수정

--- Fire_Station.py
+++ Fire_Station.py
@@ -73,31 +73,31 @@ lon_curr = 126.778892
 #         self.btn_map.grid(row= 0 , column = 0)
 #         self.btn_start.grid(row= 1 , column = 0)
      
-def geocoding_reverse(lat, lon): 
-    geolocoder = Nominatim(user_agent = 'South Korea', timeout=None)
-    address = geolocoder.reverse(f"{lat}, {lon}")
+# def geocoding_reverse(lat, lon): 
+#     geolocoder = Nominatim(user_agent = 'South Korea', timeout=None)
+#     address = geolocoder.reverse(f"{lat}, {lon}")
 
-    return address
+#     return address
 
-def mqtt_s():
-    s_st.loop_forever()
+# def mqtt_s():
+#     s_st.loop_forever()
     
-def call_back(client, userdata, message):
-    string = str(message.payload.decode("utf-8"))
-    command, latitude, longitude = string.split(",")
-    address = geocoding_reverse(latitude, longitude)
-    lat = float(latitude)
-    lon = float(longitude)
-    lat_curr = 37.670806
-    lon_curr = 126.778892
-    m = folium.Map(location=[(lat+lat_curr)/2, (lon+lon_curr)/2], zoom_start=15)
-    folium.Marker([lat_curr, lon_curr], popup='<b>Fire Station</b>').add_to(m)
-    folium.Marker([lat, lon], popup='<b>Destination</b>').add_to(m)
-    m.save("map.html")
-    webbrowser.open("map.html")
-    lb_lat_curr.config(text=f"{lat}")
-    lb_lon_curr.config(text=f"{lon}")
-    lb_adress_curr.config(text = address)
+# def call_back(client, userdata, message):
+#     string = str(message.payload.decode("utf-8"))
+#     command, latitude, longitude = string.split(",")
+#     address = geocoding_reverse(latitude, longitude)
+#     lat = float(latitude)
+#     lon = float(longitude)
+#     lat_curr = 37.670806
+#     lon_curr = 126.778892
+#     m = folium.Map(location=[(lat+lat_curr)/2, (lon+lon_curr)/2], zoom_start=15)
+#     folium.Marker([lat_curr, lon_curr], popup='<b>Fire Station</b>').add_to(m)
+#     folium.Marker([lat, lon], popup='<b>Destination</b>').add_to(m)
+#     m.save("map.html")
+#     webbrowser.open("map.html")
+#     lb_lat_curr.config(text=f"{lat}")
+#     lb_lon_curr.config(text=f"{lon}")
+#     lb_adress_curr.config(text = address)
     # if(command == "report"):
     #     msg = messagebox.askquestion("report", "there is report\ndo you start mission?")
     #     if(msg == "yes"):
@@ -140,60 +140,115 @@ def call_back(client, userdata, message):
     # # 거리 계산
     # haversine(Seoul, Toronto, unit = 'km')
     
-broker = "210.106.192.242"
-s_st = mqtt.Client("mqtt_ST")
-s_st.on_message = call_back
+# broker = "210.106.192.242"
+# s_st = mqtt.Client("mqtt_ST")
+# s_st.on_message = call_back
  
     
-s_st.connect(broker, 1883)
-s_st.subscribe("data/ST")
+# s_st.connect(broker, 1883)
+# s_st.subscribe("data/ST")
             
-               
+            # lb_lat_curr.insert(0, "asd")   
 root = Tk()
 root.title('GUI')
-frame1 = Frame(root, width=500, height=500,borderwidth=10)
-frame2 = Frame(root, width=500, height=500,borderwidth=10)
+root.geometry("600x500")
 
-# frame1
-lb_lat = Label(frame1, text="Latitude", width=10, height=3, bg='white', fg='black', relief='ridge', borderwidth=4)
-lb_lon = Label(frame1, text="Longitude", width=10, height=3, bg='white', fg='black', relief='ridge', borderwidth=4)
-lb_lat_curr = Label(frame1, text="-", width=20, height=3, bg='white', fg='black', relief='ridge', borderwidth=4)
-lb_lon_curr = Label(frame1, text="-", width=20, height=3, bg='white', fg='black', relief='ridge', borderwidth=4)
-lb_state_curr = Label(frame1, text="Not Yet", width=20, height=3, bg='white', fg='black', relief='ridge', borderwidth=4)
-lb_state = Label(frame1, text="Drone", width=10, height=3, bg='white', fg='black', relief='ridge', borderwidth=4)
-lb_adress = Label(frame1, text="Address", width=10, height=3, bg='white', fg='black', relief='ridge', borderwidth=4)
-lb_adress_curr = Label(frame1, text="-", width=20, height=3, bg='white', fg='black', relief='ridge', borderwidth=4)
 
-lb_lat.grid(row=0,column=0)
-lb_lat_curr.grid(row=0,column=1)
-        
-lb_lon.grid(row=1,column=0)
-lb_lon_curr.grid(row=1,column=1)
+lbFrame_place = LabelFrame(root, text = "위치 정보", pady = 10, padx = 20)
 
-lb_adress.grid(row=2,column=0)
-lb_adress_curr.grid(row=2,column=1)
-        
-lb_state.grid(row=3,column=0)
-lb_state_curr.grid(row=3,column=1)
+locationFrame = Frame(lbFrame_place, width=500, height=500, padx=10)
+addressFrame = Frame(locationFrame, width=500, height=500)
+buttonFrame = Frame(lbFrame_place, width=500, height=500, pady=20)
+
+
+lb_lat = Label(locationFrame, text="경도", width=10, height=2)
+lb_lat_curr = Entry(locationFrame, width=20)
+
+lb_lat_curr['state'] = "readonly"
+lb_lat_curr['readonlybackground'] = "white"
+
+lb_lat.grid(row=0, column=0)
+lb_lat_curr.grid(row=0, column=1, sticky='w')
 
 
 
-#frame2
-btn_map = Button(frame2, text="지도보기",  pady = 5, padx = 5)
-btn_start = Button(frame2, text="드론 출발",  pady = 5, padx = 5)
-btn_cancel = Button(frame2, text='취소', pady = 5, padx = 5)
-        
-btn_map.grid(row= 0 , column = 0,  pady = 5, padx = 5)
-btn_start.grid(row= 2 , column = 0,  pady = 5, padx = 5)
-btn_cancel.grid(row=2, column=1,  pady = 5, padx = 5)
+lb_lon = Label(locationFrame, text="위도", width=10, height=2)
+lb_lon_curr = Entry(locationFrame, width = 20)
 
-btn_start["state"] = DISABLED
-btn_cancel["state"] = DISABLED
+lb_lon_curr['state'] = "readonly"
+lb_lon_curr['readonlybackground'] = "white"
 
-frame1.pack(side='left')
-frame2.pack(side='left')
-t2 = threading.Thread(target=mqtt_s)
-t2.start()
+lb_lon.grid(row=1, column=0)
+lb_lon_curr.grid(row=1, column=1, sticky='w')
+
+
+
+lb_dist = Label(locationFrame, text="거리", width=10, height=2)
+lb_dist_curr = Entry(locationFrame, width = 20)
+
+lb_dist_curr['state'] = "readonly"
+lb_dist_curr['readonlybackground'] = "white"
+
+lb_dist.grid(row=2, column=0)
+lb_dist_curr.grid(row=2, column=1, sticky='w')
+
+
+
+lb_address = Label(locationFrame, text="주소", width=10, height=2)
+lb_address_curr = Entry(locationFrame, text="-", width =52)
+
+lb_address_curr['state'] = "readonly"
+lb_address_curr['readonlybackground'] = "white"
+
+lb_address.grid(row=3, column=0)
+lb_address_curr.grid(row=3, column=1)
+
+
+locationFrame.pack(side='top')
+
+
+btn_map = Button(buttonFrame, text="지도보기",width=10)
+btn_start = Button(buttonFrame, text="드론 보내기",width=10)
+btn_cancel = Button(buttonFrame, text="취소하기", width=10)
+
+btn_map['state'] = DISABLED
+btn_start['state'] = DISABLED
+btn_cancel['state'] = DISABLED
+
+btn_start.grid(row=0, column=0, padx=10, ipadx=5)
+btn_cancel.grid(row=0, column=1, padx=10, ipadx=5)
+btn_map.grid(row=0, column=2, padx=10, ipadx=5)
+
+buttonFrame.pack(side='top')
+
+lbFrame_place.pack(side='top')
+
+
+
+
+lbFrame_state = LabelFrame(root, text = "드론 상태", padx = 20, pady = 20)
+
+lb_mission_curr = Label(lbFrame_state, text="X")
+lb_mission = Label(lbFrame_state, text="미션 수행 중")
+
+lb_mission.pack(side='left')
+lb_mission_curr.pack(side='left')
+
+
+lb_arrive_curr = Label(lbFrame_state, text="X")
+lb_arrive = Label(lbFrame_state, text="드론 도착")
+
+lb_arrive.pack(side='left')
+lb_arrive_curr.pack(side='left')
+
+
+lbFrame_state.pack(side='top')
+
+
+btn_end = Button(root, text='종료', pady = 10, padx = 10)
+btn_end.pack(side='bottom', anchor=E, pady = 20, padx = 20)
+# t2 = threading.Thread(target=mqtt_s)
+# t2.start()
     
 root.mainloop()
-t2.join()
+# t2.join()
